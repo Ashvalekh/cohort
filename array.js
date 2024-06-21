498708e8e9f6bef560b531a7b4454799d4dd7185
Ashva.stack <152001841+Ashvalekh@users.noreply.github.com>
2024-06-21 22:06:13 +0000

    array

---
+++ array.js
@@ -0,0 +1,13 @@
+// let a1 = [1,2,3,4]
+// let a2 = [5,6,7,8]
+
+// console.log(a1.concat(a2))
+
+let a = [1,2,3]
+// for(let i=0; i<a.length; i++){
+//     console.log(a[i])
+// }
+function logthing(str){
+    console.log(str)
+}
+a.forEach(logthing)
