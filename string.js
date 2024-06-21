73b5e6b8606e77a8b7b9fb5bafeb4f12e44778e3
Ashva.stack <152001841+Ashvalekh@users.noreply.github.com>
2024-06-21 21:36:51 +0000

    string

---
+++ string.js
@@ -0,0 +1,33 @@
+// function getLength(str) {
+//     console.log("Original String:", str);
+//     console.log("Length:", str.length);
+//   }
+//   getLength("Hello World");
+
+// indexOf
+// function jagah(str, target){
+//   console.log("The string is : " , str)
+//   console.log("index:", str.indexOf(target))
+// }
+
+//  jagah("Hello World", "o")
+
+// slice //substr
+// function katde(str, start, end){
+//   console.log("Og string: ", str)
+//   console.log("yaha se katto : " , str.slice(start, end))
+//   console.log("yaha se katto2 : " , str.substr(start, end))
+// }
+
+// katde("Hello World", 1,7)
+
+//split //replace
+// let value = "Hi i am ashutosh"
+// let v = value.split(" ")
+// let b = value.replace("Hi", "Konichiwa")
+// console.log(b)
+
+//trim
+
+let value = "   Ashutosh Singh    "
+console.log(value.trim())
