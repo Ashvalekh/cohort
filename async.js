7c0ed2bbadd9268946b5a847d88f1d6e93bed81d
Ashva.stack <152001841+Ashvalekh@users.noreply.github.com>
2024-06-22 21:40:51 +0000

    asdf

---
+++ async.js
@@ -0,0 +1,28 @@
+
+// function findSum(n){
+//     let ans = 0;
+//     for(let i = 1; i<=n; i++){
+        
+//         ans = ans + i;
+//     }
+//     return ans;
+    
+// }
+
+// function waitTill(){
+//     console.log(findSum(100))
+// }
+
+// setTimeout(waitTill,3000)
+// console.log("hello world")
+
+// rading a file using fs.readfile
+
+// const fs = require("fs");
+
+// fs.readFile("a.txt", "utf-8", function(err, data){
+//     console.log(data)
+// })
+
+// console.log("holla amigos")
+
