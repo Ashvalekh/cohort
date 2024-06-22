d17f960235e059aa01a6c86acb62f8634b829aa8
Ashva.stack <152001841+Ashvalekh@users.noreply.github.com>
2024-06-22 22:26:11 +0000

    45

--- async.js
+++ async.js
@@ -26,3 +26,58 @@
 
 // console.log("holla amigos")
 
+//promises
+
+// const fs = require("fs")
+
+// function ahureads(){
+//     return new Promise(function(resolve){
+//         fs.readFile("a.txt", "utf-8", function(err, data){
+//             resolve(data)
+//         })
+//     })
+// }
+
+// function onDone(data){
+//     console.log(data)
+// }
+
+// ahureads().then(onDone)
+
+// var d = new Date();
+
+// console.log(d.getDate())
+
+// let p = new Promise(function(resolve){
+//     resolve("hi there")
+// })
+// console.log("teri maa ki")
+// p.then(function(){
+//     console.log(p)
+// })
+
+// console.log("ooski maaki")
+
+function ashuAsync(){
+    let p = new Promise(function(resolve){
+        //do some async logic here
+        setTimeout(function(){
+
+           resolve( "hi there!")
+        }, 3000)
+    })
+    return p
+}
+
+async function main(){
+    // ashuAsync().then(function(value){
+    //     console.log(value)
+    // })
+    let value = await ashuAsync();
+    console.log(value)
+    console.log("hi there 1")
+    //  console.log(value)
+
+}
+
+main()
