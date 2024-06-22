82baf9a75e63c7cdace2968659c70e5afeeb4ec8
Ashva.stack <152001841+Ashvalekh@users.noreply.github.com>
2024-06-22 19:55:59 +0000

    anonymous

--- function.js
+++ function.js
@@ -1,9 +1,42 @@
-function findsum(n){
-    let ans = 0;
-    for (let i = 1; i<=n; i++){
-        ans = ans+i;
-    }
-    console.log(ans);
+// function findsum(n){
+//     let ans = 0;
+//     for (let i = 1; i<=n; i++){
+//         ans = ans+i;
+//     }
+//     console.log(ans);
+// }
+
+// findsum(9)
+
+// function square(n){
+//     return n*n
+// }
+
+// function sumOfSquare(a,b){
+//     let c = square(a)
+//     let d = square(b)
+//     return c+d
+// }
+
+// console.log(sumOfSquare(2,4))
+
+// CallBack
+
+function square(a){
+    return a*a
+} 
+
+function cube(a){
+    return a*a*a
 }
 
-findsum(9)
+function sumOfsomething(b,c,fn){
+    console.log(fn)
+    let val1 = fn(b)
+    let val2 = fn(c)
+    return val1+val2
+}
+
+console.log(sumOfsomething(3,4,function (a){
+    return a*a*a
+}))
