b0969104e3da99b913b46c391076ff196b5f37c4
Ashva.stack <152001841+Ashvalekh@users.noreply.github.com>
2024-06-24 20:12:12 +0000

    sad

---
+++ async/async.js
@@ -0,0 +1,13 @@
+let a = 0;
+console.log(a)
+
+fs.readFile("a.txt", "utf-8", function(err,data){
+    console.log("this is in the data",data)
+})
+
+    let ans = 0
+    for(let i=0; i<=10000; i++){
+        ans= ans+i
+    }
+
+    console.log(ans)
