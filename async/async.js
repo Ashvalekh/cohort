5003a4b1841033a8244be88f83b31918046e41ba
Ashva.stack <152001841+Ashvalekh@users.noreply.github.com>
2024-06-24 21:38:38 +0000

    qqqq

--- async/async.js
+++ async/async.js
@@ -1,13 +1,26 @@
-let a = 0;
-console.log(a)
+// var fs = require('fs');
 
-fs.readFile("a.txt", "utf-8", function(err,data){
-    console.log("this is in the data",data)
-})
+// let a = 0;
+// console.log(a)
 
-    let ans = 0
-    for(let i=0; i<=10000; i++){
-        ans= ans+i
-    }
+// fs.readFile("a.txt", "utf-8", function(err,data){
+//     console.log("this is in the data: ",data)
+    
+// })
 
-    console.log(ans)
+//     let ans = 0
+//     for(let i=0; i<=10000; i++){
+//         ans= ans+i
+//     }
+
+//     console.log(ans)
+
+//  promises
+
+function myownsettimeout(callback, duration){
+    setTimeout(callback, duration)
+}
+
+myownsettimeout(function(){
+    console.log("afterset timeout")
+}, 1000)
